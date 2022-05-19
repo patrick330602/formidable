d12a95f9a3a31ec860b358f682a555a7b862eb6a
Cyril Walle <cyril.walle@protonmail.com>
2022-05-19 11:48:46 +0200

    feat: stop at first invalid char

--- src/Formidable.js
+++ src/Formidable.js
@@ -44,9 +44,9 @@ function hasOwnProp(obj, key) {
   return Object.prototype.hasOwnProperty.call(obj, key);
 }
 
-const validExtensionChar = (c) => {
+const invalidExtensionChar = (c) => {
   const code = c.charCodeAt(0);
-  return (
+  return !(
     code === 46 || // .
     (code >= 65 && code <= 90) ||
     (code >= 97 && code <= 122)
@@ -550,7 +550,11 @@ class IncomingForm extends EventEmitter {
       rawExtname =  basename.slice(firstDot);
     }
 
-    return Array.from(rawExtname).filter(validExtensionChar).join('');
+    const firstInvalidIndex = Array.from(rawExtname).findIndex(invalidExtensionChar);
+    if (firstInvalidIndex === -1) {
+      return rawExtname;
+    }
+    return rawExtname.substring(0, firstInvalidIndex);
   }
 
 
